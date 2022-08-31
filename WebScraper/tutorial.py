ced13c6201c85028e61ffa0184189753ee2bbeb1
Cameron Thomas <cameron102601@gmail.com>
2022-08-30 21:08:22 -0500

    Created functions to pull a list of options

--- WebScraper/tutorial.py
+++ WebScraper/tutorial.py
@@ -1,10 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://boss.latech.edu/ia-bin/tsrvweb.cgi?tserve_tip_read_destroy&tserve_tip_write=||WID|SID|PIN|Term|AwdYear|AdTyCode|ConfigName&ConfigName=rcrssect1&ReqNum=1&tserve_trans_config=rcrssect1.cfg&tserve_host_code=HostZero&tserve_tiphost_code=TipZero&dt=1661905508000'
-response = requests.get(url)
+# Grabs the list of options from a select element in HTML
+def pull_options_list(url):
+    response = requests.get(url)
 
-soup = BeautifulSoup(response.text, 'html.parser')
-pageText = soup.text
+    soup = BeautifulSoup(response.text, 'html.parser')
+    options = soup.select('select[name=Subject] > option')
+    for x in options:
+        options_text = x.getText()
+    options_list = options_text.replace("\r", "").split("\n")
+    clean_options_list = [i for i in options_list if i != '']
+    clean_options_list = clean_options_list[:-1]
 
-print(pageText)
+    return clean_options_list
+
+print(pull_options_list('https://boss.latech.edu/ia-bin/tsrvweb.cgi?tserve_trans_config=rcolcat1hp.cfg&tserve_tip_write=||WID&tserve_host_code=HostZero&tserve_tiphost_code=TipZero'))
