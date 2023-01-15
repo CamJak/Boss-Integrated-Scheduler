11a9512ecf9c5d82af0287adf6238f0b6f413e92
JingusJohn <branchjohnchristopher@gmail.com>
2023-01-15 12:57:47 -0600

    formatted some files

--- App/tailwind.config.cjs
+++ App/tailwind.config.cjs
@@ -1,14 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {
-      fontFamily: {
-        poppins: ['Poppins', 'sans-serif']
-      }
-    },
-  },
-  plugins: [
-    require("@tailwindcss/typography")
-  ],
-}
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				poppins: ['Poppins', 'sans-serif']
+			}
+		}
+	},
+	plugins: [require('@tailwindcss/typography')]
+};
