00687f51295d1288c27f875a79506dd00fdf6792
JingusJohn <branchjohnchristopher@gmail.com>
2023-04-12 10:27:18 -0500

    cleaner now returns data to caller. database.py created

---
+++ WebScraper/database.py
@@ -0,0 +1,16 @@
+
+def insert_to_database(cleaned_data={}):
+
+    if cleaned_data == {}:
+        # if the data is not provided, check for the cleaned output json
+        pass
+
+    # at this point, cleaned_data should be defined in some way
+    
+    # take that cleaned data and create instances of some basic dataclasses
+    #  to then easily insert that data into the corresponding tabes in the database
+
+
+
+if __name__ == "__main__":
+    insert_to_database()
--- WebScraper/main.py
+++ WebScraper/main.py
@@ -1,7 +1,9 @@
 from seleniumNavigator import run_scraper
 from dataCleaner import clean
+from database import insert_to_database
 
 # this means it only runs when directly executed by python
 if __name__ == "__main__":
-    scraperOut = run_scraper('Spring 2023')
-    clean(scraperOut)
+    scraper_out = run_scraper('Spring 2023')
+    cleaner_out = clean(scraper_out)
+    insert_to_database(cleaner_out)
